bf7de92a099b0d13716db956d936792c3f4d38f6
CuongNN218 <26113323+CuongNN218@users.noreply.github.com>
2023-06-12 16:51:25 -0400

    Create reg_score.py
    add code for transferability score calculation

---
+++ reg_score.py
@@ -0,0 +1,30 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import mean_squared_error
+
+def transferability_score(y_source, y_target):
+    '''
+    Args: 
+      y_source (ndarray): extracted features of target samples, pseudo source labels or source labels (N x D1)
+      y_target (ndarray): ground truth labels (N x D2)
+    Return: 
+      mse: a scalar for transferability score 
+    '''
+    linear_reg = LinearRegression().fit(y_source, y_target)
+    y_pred = linear_reg.predict(y_source)
+    mse = -1.0 * mean_squared_error(y_target, y_pred, squared=True)
+    return mse
+
+def transferability_score_ridge(y_source, y_target, alpha):
+    '''
+    Args: 
+      y_source (ndarray): extracted features of target samples, pseudo source labels or source labels (N x D1)
+      y_target (ndarray): ground truth labels (N x D2)
+    Return: 
+      mse: a scalar for transferability score 
+    '''
+    n = y_source.shape[0]
+    ridge = Ridge(alpha=n * alpha,solver=solver).fit(y_source, y_target)
+    y_pred = ridge.predict(y_source)
+    mse = -1.0 * mean_squared_error(y_target, y_pred, squared=True)
+    return mse
